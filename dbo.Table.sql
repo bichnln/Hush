153211a5ccafff5b4c3a8b8bcb01b9ce5210e1d1
bichnln <bich.nln@gmail.com>
2018-10-12 19:12:53 +1100

    add records inserting function

---
+++ dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [ID]          INT          NOT NULL,
+    [Username]    VARCHAR (50) NOT NULL,
+    [Password]    VARCHAR (50) NOT NULL,
+    [Service]     VARCHAR (50) NOT NULL,
+    [Email]       VARCHAR (50) NULL,
+    [PhoneNumber] NCHAR (10)   NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
