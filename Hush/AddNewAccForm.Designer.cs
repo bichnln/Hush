11983f9484a5f8f140cfda9aac3231c08b76c85e
Panvina <42162390+Panvina@users.noreply.github.com>
2018-10-21 18:43:29 +1100

    Add files via upload

--- Hush/AddNewAccForm.Designer.cs
+++ Hush/AddNewAccForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddNewAccForm));
             this.CancelBtn = new System.Windows.Forms.Button();
             this.SaveBtn = new System.Windows.Forms.Button();
             this.UsrnameTBox = new System.Windows.Forms.TextBox();
@@ -50,6 +51,8 @@
             // 
             // CancelBtn
             // 
+            this.CancelBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CancelBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.CancelBtn.Location = new System.Drawing.Point(267, 263);
             this.CancelBtn.Name = "CancelBtn";
             this.CancelBtn.Size = new System.Drawing.Size(75, 23);
@@ -60,7 +63,9 @@
             // 
             // SaveBtn
             // 
-            this.SaveBtn.Location = new System.Drawing.Point(151, 263);
+            this.SaveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.SaveBtn.Location = new System.Drawing.Point(146, 263);
             this.SaveBtn.Name = "SaveBtn";
             this.SaveBtn.Size = new System.Drawing.Size(75, 23);
             this.SaveBtn.TabIndex = 1;
@@ -79,9 +84,12 @@
             // PasswrdLabel
             // 
             this.PasswrdLabel.AutoSize = true;
-            this.PasswrdLabel.Location = new System.Drawing.Point(66, 115);
+            this.PasswrdLabel.BackColor = System.Drawing.Color.Transparent;
+            this.PasswrdLabel.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswrdLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.PasswrdLabel.Location = new System.Drawing.Point(73, 105);
             this.PasswrdLabel.Name = "PasswrdLabel";
-            this.PasswrdLabel.Size = new System.Drawing.Size(53, 13);
+            this.PasswrdLabel.Size = new System.Drawing.Size(60, 17);
             this.PasswrdLabel.TabIndex = 4;
             this.PasswrdLabel.Text = "Password";
             this.PasswrdLabel.Click += new System.EventHandler(this.PasswrdLabel_Click);
@@ -89,9 +97,12 @@
             // EmailLabel
             // 
             this.EmailLabel.AutoSize = true;
-            this.EmailLabel.Location = new System.Drawing.Point(14, 194);
+            this.EmailLabel.BackColor = System.Drawing.Color.Transparent;
+            this.EmailLabel.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmailLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.EmailLabel.Location = new System.Drawing.Point(20, 184);
             this.EmailLabel.Name = "EmailLabel";
-            this.EmailLabel.Size = new System.Drawing.Size(97, 13);
+            this.EmailLabel.Size = new System.Drawing.Size(113, 17);
             this.EmailLabel.TabIndex = 6;
             this.EmailLabel.Text = "Email Registeration";
             this.EmailLabel.Click += new System.EventHandler(this.EmailLabel_Click);
@@ -99,37 +110,40 @@
             // PNumberLabel
             // 
             this.PNumberLabel.AutoSize = true;
-            this.PNumberLabel.Location = new System.Drawing.Point(40, 230);
+            this.PNumberLabel.BackColor = System.Drawing.Color.Transparent;
+            this.PNumberLabel.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PNumberLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.PNumberLabel.Location = new System.Drawing.Point(38, 220);
             this.PNumberLabel.Name = "PNumberLabel";
-            this.PNumberLabel.Size = new System.Drawing.Size(81, 13);
+            this.PNumberLabel.Size = new System.Drawing.Size(95, 17);
             this.PNumberLabel.TabIndex = 7;
             this.PNumberLabel.Text = "Phone Number ";
             this.PNumberLabel.Click += new System.EventHandler(this.PNumberLabel_Click);
             // 
             // PasswrdTBox
             // 
-            this.PasswrdTBox.Location = new System.Drawing.Point(146, 108);
+            this.PasswrdTBox.Location = new System.Drawing.Point(146, 104);
             this.PasswrdTBox.Name = "PasswrdTBox";
             this.PasswrdTBox.Size = new System.Drawing.Size(196, 20);
             this.PasswrdTBox.TabIndex = 8;
             // 
             // ServiceTBox
             // 
-            this.ServiceTBox.Location = new System.Drawing.Point(146, 151);
+            this.ServiceTBox.Location = new System.Drawing.Point(146, 144);
             this.ServiceTBox.Name = "ServiceTBox";
             this.ServiceTBox.Size = new System.Drawing.Size(196, 20);
             this.ServiceTBox.TabIndex = 9;
             // 
             // EmailTBox
             // 
-            this.EmailTBox.Location = new System.Drawing.Point(146, 187);
+            this.EmailTBox.Location = new System.Drawing.Point(146, 183);
             this.EmailTBox.Name = "EmailTBox";
             this.EmailTBox.Size = new System.Drawing.Size(196, 20);
             this.EmailTBox.TabIndex = 10;
             // 
             // PNumberTBox
             // 
-            this.PNumberTBox.Location = new System.Drawing.Point(146, 223);
+            this.PNumberTBox.Location = new System.Drawing.Point(146, 219);
             this.PNumberTBox.Name = "PNumberTBox";
             this.PNumberTBox.Size = new System.Drawing.Size(196, 20);
             this.PNumberTBox.TabIndex = 11;
@@ -137,9 +151,12 @@
             // ServiceLabel
             // 
             this.ServiceLabel.AutoSize = true;
-            this.ServiceLabel.Location = new System.Drawing.Point(76, 158);
+            this.ServiceLabel.BackColor = System.Drawing.Color.Transparent;
+            this.ServiceLabel.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ServiceLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ServiceLabel.Location = new System.Drawing.Point(86, 145);
             this.ServiceLabel.Name = "ServiceLabel";
-            this.ServiceLabel.Size = new System.Drawing.Size(43, 13);
+            this.ServiceLabel.Size = new System.Drawing.Size(47, 17);
             this.ServiceLabel.TabIndex = 12;
             this.ServiceLabel.Text = "Service";
             this.ServiceLabel.Click += new System.EventHandler(this.ServiceLabel_Click);
@@ -147,9 +164,12 @@
             // UsernameLabel
             // 
             this.UsernameLabel.AutoSize = true;
-            this.UsernameLabel.Location = new System.Drawing.Point(66, 69);
+            this.UsernameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.UsernameLabel.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UsernameLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.UsernameLabel.Location = new System.Drawing.Point(70, 67);
             this.UsernameLabel.Name = "UsernameLabel";
-            this.UsernameLabel.Size = new System.Drawing.Size(55, 13);
+            this.UsernameLabel.Size = new System.Drawing.Size(63, 17);
             this.UsernameLabel.TabIndex = 13;
             this.UsernameLabel.Text = "Username";
             this.UsernameLabel.Click += new System.EventHandler(this.UsernameLabel_Click);
@@ -172,7 +192,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(418, 335);
+            this.BackgroundImage = global::Hush.Properties.Resources.ws_Minimal_Gray_to_White_Gradient_1920x1200;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(409, 331);
             this.Controls.Add(this.UsernameLabel);
             this.Controls.Add(this.ServiceLabel);
             this.Controls.Add(this.PNumberTBox);
@@ -185,6 +207,8 @@
             this.Controls.Add(this.UsrnameTBox);
             this.Controls.Add(this.SaveBtn);
             this.Controls.Add(this.CancelBtn);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "AddNewAccForm";
             this.Text = "AddNewAccForm";
             this.Load += new System.EventHandler(this.AddNewAccForm_Load);
